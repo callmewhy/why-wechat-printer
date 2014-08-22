ef9c6167ff8a7e65af7f240fb1729d8771f4c2c4
Hyde Wang <wanghaiyang@139.me>
2014-08-22 22:25:02 +0800

    add c# project

---
+++ c#/WeixinPrinter/Controller/WxPrinter.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeixinPrinter
+{
+    /// <summary>
+    /// 控制器，微信打印机的封装类，处理逻辑，例如HTTP访问，打印照片等等
+    /// </summary>
+    class WxPrinter
+    {
+        public static string API_URL = "http://wxprinter.sinaapp.com/pcapi/";
+        public static string RESET_PRINTER_CODE_URL = API_URL + "?action=reset_code";
+        public static string GET_TASK_IMAGE_URL = API_URL + "?action=get_task";
+
+        public bool isPrinting = false; //目前打印机的状态，是否在打印
+        HomeForm myHomeForm;            //主页面
+        ImageTool myImageTool;          //图片加载的工具包
+        PrintService myPrintServer;       //打印用的工具包
+
+        // WxPrinter的构造方法
+        public WxPrinter(HomeForm homeForm)
+        {
+            myHomeForm = homeForm;
+            myPrintServer = new PrintService(this);
+            myImageTool = new ImageTool(this);
+        }
+
+        //---------- print_server api ----------
+        //打印事件结束了
+        public void endPrint()
+        {
+            isPrinting = false;
+        }
+
+        //---------- image_tool api ----------
+        //监测到了新的照片的处理
+        public void rcvdNewImage(string imgUrl)
+        {
+            isPrinting = true;
+
+            //定义下载的文件名称和路径
+            string filePath = System.Windows.Forms.Application.UserAppDataPath+"/down_image.why";
+            //1.下载
+            ImageTool.DownloadFile(imgUrl,filePath);
+
+            //2.展示
+            setShowImage(filePath);
+
+            //3.打印
+            myPrintServer.printImage(filePath);
+            //4.事后
+
+        }
+
+        //设置主页面的展示图片
+        private void setShowImage(string imgUrl)
+        {
+            myHomeForm.setShowImage(imgUrl);
+        }
+
+
+        //---------- homepage api ----------
+        //开始监测打印任务
+        public void startCheckTask()
+        {
+            myImageTool.startCheckTask();//开始监测待打印的照片
+        }
+
+        //重置打印码，将原来的print_code设为未用，并选择新的print_code设为已用
+        public void resetPrintCode()
+        {
+            string old_code = getPrintCode();
+            string new_code = HttpTool.sendPost(RESET_PRINTER_CODE_URL, "print_code=" + old_code);
+            setPrintCode(new_code);
+        }
+        //获取setting中的print_code
+        public string getPrintCode()
+        {
+            return Properties.Settings.Default.print_code;
+        }
+        //设置setting中的print_code
+        private void setPrintCode(string print_code)
+        {
+            Properties.Settings.Default.print_code = print_code;
+            logLog("print_code=" + print_code);
+            Properties.Settings.Default.Save();
+        }
+
+
+        //---------- 测试函数 ----------
+        private static void logLog(String logStr)
+        {
+            Console.WriteLine("-----WHY-----" + logStr);
+        }
+
+    }
+}
---
+++ c#/WeixinPrinter/View/Homepage.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WeixinPrinter
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeForm));
+            this.lDetailPictrueBox = new System.Windows.Forms.PictureBox();
+            this.rPanel = new System.Windows.Forms.Panel();
+            this.rPrintCodeLabel = new System.Windows.Forms.Label();
+            this.rQRcodePictrueBox = new System.Windows.Forms.PictureBox();
+            this.rIntroPicture = new System.Windows.Forms.PictureBox();
+            this.lPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.lDetailPictrueBox)).BeginInit();
+            this.rPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rQRcodePictrueBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rIntroPicture)).BeginInit();
+            this.lPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lDetailPictrueBox
+            // 
+            this.lDetailPictrueBox.Image = ((System.Drawing.Image)(resources.GetObject("lDetailPictrueBox.Image")));
+            this.lDetailPictrueBox.Location = new System.Drawing.Point(55, 52);
+            this.lDetailPictrueBox.Margin = new System.Windows.Forms.Padding(0);
+            this.lDetailPictrueBox.Name = "lDetailPictrueBox";
+            this.lDetailPictrueBox.Size = new System.Drawing.Size(583, 343);
+            this.lDetailPictrueBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.lDetailPictrueBox.TabIndex = 0;
+            this.lDetailPictrueBox.TabStop = false;
+            // 
+            // rPanel
+            // 
+            this.rPanel.BackColor = System.Drawing.Color.Transparent;
+            this.rPanel.Controls.Add(this.rPrintCodeLabel);
+            this.rPanel.Controls.Add(this.rQRcodePictrueBox);
+            this.rPanel.Controls.Add(this.rIntroPicture);
+            this.rPanel.Location = new System.Drawing.Point(716, 0);
+            this.rPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.rPanel.Name = "rPanel";
+            this.rPanel.Size = new System.Drawing.Size(168, 450);
+            this.rPanel.TabIndex = 2;
+            // 
+            // rPrintCodeLabel
+            // 
+            this.rPrintCodeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.rPrintCodeLabel.Font = new System.Drawing.Font("微软雅黑", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rPrintCodeLabel.Location = new System.Drawing.Point(15, 225);
+            this.rPrintCodeLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.rPrintCodeLabel.Name = "rPrintCodeLabel";
+            this.rPrintCodeLabel.Size = new System.Drawing.Size(150, 30);
+            this.rPrintCodeLabel.TabIndex = 2;
+            this.rPrintCodeLabel.Text = "2333";
+            this.rPrintCodeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.rPrintCodeLabel.UseMnemonic = false;
+            this.rPrintCodeLabel.Click += new System.EventHandler(this.rPrintCodeLabel_Click);
+            // 
+            // rQRcodePictrueBox
+            // 
+            this.rQRcodePictrueBox.Location = new System.Drawing.Point(20, 10);
+            this.rQRcodePictrueBox.Margin = new System.Windows.Forms.Padding(0);
+            this.rQRcodePictrueBox.Name = "rQRcodePictrueBox";
+            this.rQRcodePictrueBox.Size = new System.Drawing.Size(130, 130);
+            this.rQRcodePictrueBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rQRcodePictrueBox.TabIndex = 0;
+            this.rQRcodePictrueBox.TabStop = false;
+            // 
+            // rIntroPicture
+            // 
+            this.rIntroPicture.Image = ((System.Drawing.Image)(resources.GetObject("rIntroPicture.Image")));
+            this.rIntroPicture.Location = new System.Drawing.Point(20, 150);
+            this.rIntroPicture.Name = "rIntroPicture";
+            this.rIntroPicture.Size = new System.Drawing.Size(130, 284);
+            this.rIntroPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.rIntroPicture.TabIndex = 3;
+            this.rIntroPicture.TabStop = false;
+            // 
+            // lPanel
+            // 
+            this.lPanel.Controls.Add(this.lDetailPictrueBox);
+            this.lPanel.Location = new System.Drawing.Point(-1, 0);
+            this.lPanel.Name = "lPanel";
+            this.lPanel.Size = new System.Drawing.Size(714, 465);
+            this.lPanel.TabIndex = 3;
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WeixinPrinter.Properties.Resources.retina_wood;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.lPanel);
+            this.Controls.Add(this.rPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "HomeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "微信打印机";
+            this.Shown += new System.EventHandler(this.HomeForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.lDetailPictrueBox)).EndInit();
+            this.rPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.rQRcodePictrueBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rIntroPicture)).EndInit();
+            this.lPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox lDetailPictrueBox;
+        private System.Windows.Forms.Panel rPanel;
+        private System.Windows.Forms.PictureBox rQRcodePictrueBox;
+        private System.Windows.Forms.Label rPrintCodeLabel;
+        private System.Windows.Forms.Panel lPanel;
+        private System.Windows.Forms.PictureBox rIntroPicture;
+    }
+}
+
